b5ba1813d5d97ce68db25218125c4b3466b16a08
JuanAlejandroCuadrosRodriguez <u20221a359@upc.edu.pe>
2025-07-01 08:27:19 -0500

    fix: code refactor

--- src/main/java/com/backend/hormonalcare/profile/domain/model/queries/GetProfileByNameQuery.java
+++ src/main/java/com/backend/hormonalcare/profile/domain/model/queries/GetProfileByNameQuery.java
@@ -1,6 +1,5 @@
 package com.backend.hormonalcare.profile.domain.model.queries;
 
-import com.backend.hormonalcare.profile.domain.model.valueobjects.PersonName;
 
 public record GetProfileByNameQuery(String name) {
 }
